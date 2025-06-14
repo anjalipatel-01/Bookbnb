ec1dda96c58f34c9a5d45a293fca339f394d1235
Anjali <anjalipatel6607@gmail.com>
2025-06-14 11:37:54 +0530

    Renamed Models to models

--- models/listing.js
+++ models/listing.js
@@ -15,10 +15,6 @@ const listingSchema = new Schema({
     price:Number,
     location : String,
     country:String,
-    reviews:[{
-        type: Schema.Types.ObjectId,
-        ref : "Reviews",
-    }]
 });
 const Listing = mongoose.model("Listing",listingSchema);
 module.exports=Listing;
